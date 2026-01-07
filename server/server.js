81e999ba1c5c824cc9ee886762f9137ca10b6df2
pikatchu456 <elysemanantsoa456@gmail.com>
2026-01-07 12:53:55 +0300

    message email

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { inngest, functionna } from './inngest/index.js';
+import { inngest, functions } from './inngest/index.js';
 import { serve } from 'inngest/express';
+import {clerkMiddleware} from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -11,11 +13,11 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'))
-
-
-app.use('/api/inngest', serve({ client: inngest, functions: functionna }))
+app.use('/api/inngest', serve({ client: inngest, functions: functions }))
+app.use('/api/users', userRouter)
 
 const PORT = process.env.PORT || 4000;
 
